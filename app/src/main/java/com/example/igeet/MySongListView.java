468ce3fc295bedcaac349705db0fe40f9814d8ff
gauriyarrahul <rahulgauriyar@gmail.com>
2022-07-07 10:19:03 +0530

    music player project init

---
+++ app/src/main/java/com/example/igeet/MySongListView.java
@@ -0,0 +1,13 @@
+package com.example.igeet;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MySongListView extends RecyclerView {
+    public MySongListView(@NonNull Context context) {
+        super(context);
+    }
+
+}
